d8f7418e0836d9d5ac99c1d4db76b755ebce3e9b
Ian de Keijzer <ian.dekeijzer@hotmail.com>
2025-09-29 17:22:44 +0200

    Adding sine wave function

--- signals.py
+++ signals.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
-import matplotlib.pyplot as plt
 import numpy as np
 
 def u(start, finish,delay, amplitude):
     t= np.linspace(start,finish,1000)
     return np.where(t<0+delay, 0, amplitude)
+ 
+    
+def generate_sine_wave(frequency, duration, sample_rate):
+     t= np.linspace (0, duration, sample_rate)
+     y= np.sin(frequency*2*np.pi*t)
+     return y
+ 
+
+
 
 
 
