8c5044a92638fca2e2a48d2c5809dfba8d932f14
Ian de Keijzer <ian.dekeijzer@hotmail.com>
2025-10-01 17:19:04 +0200

    Added the unit step function with plot functions

--- run.py
+++ run.py
@@ -21,22 +21,3 @@ plt.xlabel("Time")
 plt.ylabel("Height")
 plt.show()
 
-#generate a modified sine wave with a predetermined amplitude
-t,y = sig.modified_sine_wave(5, 2, 1000,amplitude=10.0)
-plt.figure(3)
-plt.plot(t, y)
-plt.grid(True)
-plt.title("Modified Sine wave")
-plt.xlabel("Time")
-plt.ylabel("Amplitude")
-plt.show()
-
-#generate a modified unit step function with a predetermined delay
-t, y= sig.modified_u(-10,10,-2,3,10000)
-plt.figure(4)
-plt.plot(t, y)
-plt.grid(True)
-plt.title("Modified Unit step function")
-plt.xlabel("Time")
-plt.ylabel("Height")
-plt.show()
