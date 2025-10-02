a1a6a40eb4e40b2dc73dd4f8504214ae911de576
Ian de Keijzer <ian.dekeijzer@hotmail.com>
2025-10-02 20:12:48 +0200

    Removed unnecessary plt.show prompts

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ plt.grid(True)
 plt.title("Sine wave")
 plt.xlabel("Time")
 plt.ylabel("Amplitude")
-plt.show()
+
 
 #generate unit step function
 t, y= sig.u(-10,10,3,10000)
@@ -19,7 +19,7 @@ plt.grid(True)
 plt.title("Unit step function")
 plt.xlabel("Time")
 plt.ylabel("Height")
-plt.show()
+
 
 #generate a modified sine wave with a predetermined amplitude
 t,y = sig.modified_sine_wave(5, 2, 1000,amplitude=10.0)
@@ -29,7 +29,7 @@ plt.grid(True)
 plt.title("Modified Sine wave")
 plt.xlabel("Time")
 plt.ylabel("Amplitude")
-plt.show()
+
 
 #generate a modified unit step function with a predetermined delay
 t, y= sig.modified_u(-10,10,-2,3,10000)
@@ -39,4 +39,4 @@ plt.grid(True)
 plt.title("Modified Unit step function")
 plt.xlabel("Time")
 plt.ylabel("Height")
-plt.show()
+plt.show() 
