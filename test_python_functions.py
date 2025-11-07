964f8c027ed8797869c5247ecfe7ed074eecafe1
Ian de Keijzer <ian.dekeijzer@hotmail.com>
2025-11-07 14:44:16 +0100

    added final test to modified unit step function

--- test_python_functions.py
+++ test_python_functions.py
@@ -8,8 +8,6 @@ def test_generate_sine_wave():
     assert y[0] == 0
     assert np.isclose(max(y), 1, atol=1e-6)
 
-
-
 #testing modified_sine_wave function
 def test_modified_sine_wave():
     t, y = sig.modified_sine_wave(5, 2, 10000,amplitude=10.0)
@@ -22,5 +20,13 @@ def test_u():
     t, y= sig.u(-10,10,3,10000)
     assert len(t) == 10000
     assert np.isclose(max(y),3, atol=1e-6)
+    assert y[0] == 0
+    assert y[9000] == 3
+
+#testing modified unit step function
+def test_modified_u():
+    t, y= sig.modified_u(-10,10,-2,3,10000)
+    assert len(t) == 10000
+    assert np.isclose(max(y), 3, atol=1e-6)
     assert y[0] == 0
     assert y[9000] == 3
